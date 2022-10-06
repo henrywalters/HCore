c83eca1e0da7c4c0144ef56f55d752d721a4c75d
Henry Walters <henrywalters20@gmail.com>
2022-10-05 23:09:20 -0400

    Adds handle for Event Listener allowing removal

--- src/eventListenerPool.ts
+++ src/eventListenerPool.ts
@@ -1,18 +1,37 @@
 // Super simple class to allow many listeners for a single event
+
+import {Random} from "./random";
+
+export interface EventListener<T> {
+    id: string;
+    handler: (payload: T) => void
+}
+
 export default class EventListenerPool<T> {
-    private pool: Array<(payload: T) => void>;
+    private readonly pool: {[id: string]: EventListener<T>};
 
     constructor() {
-        this.pool = [];
+        this.pool = {};
     }
 
-    public listen(handler: (payload: T) => void) {
-        this.pool.push(handler);
+    public listen(handler: (payload: T) => void): EventListener<T> {
+        const listener = {
+            id: Random.alphanumeric(16),
+            handler,
+        }
+        this.pool[listener.id] = listener;
+        return listener;
+    }
+
+    public remove(listener: EventListener<T>) {
+        if (listener.id in this.pool) {
+            delete this.pool[listener.id];
+        }
     }
 
     public emit(payload: T) {
-        for (const listener of this.pool) {
-            listener(payload);
+        for (const id in this.pool) {
+            this.pool[id].handler(payload);
         }
     }
 }
